b7b9f5470e6964645205f9ea66bdeacb4c8e9789
Johan S <johansteven87@gmail.com>
2024-04-01 17:34:00 +0200

    upload data folder

---
+++ data/postgres_query.sql
@@ -0,0 +1,5 @@
+INSERT INTO pageview_counts VALUES ('Apple', 38, '2024-04-01 13:00:00+00:00');
+INSERT INTO pageview_counts VALUES ('Facebook', 404, '2024-04-01 13:00:00+00:00');
+INSERT INTO pageview_counts VALUES ('Google', 420, '2024-04-01 13:00:00+00:00');
+INSERT INTO pageview_counts VALUES ('Microsoft', 114, '2024-04-01 13:00:00+00:00');
+INSERT INTO pageview_counts VALUES ('Amazon', 4, '2024-04-01 13:00:00+00:00');
